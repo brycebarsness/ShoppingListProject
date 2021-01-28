1b59b214041bbaf9e385d9549c8e4c16f324f1e1
Milton Fernandez <miltonfernandez17@yahoo.com>
2021-01-28 14:08:24 -0600

    get route changes made a list

---
+++ src/components/GroceryList/GroceryList.jsx
@@ -0,0 +1,25 @@
+import{useState} from 'react';
+import './GroceryList.css';
+function GroceryList(){
+    
+    let [groceryArray, setGroceryArray] = useState([]);
+return(
+   
+    <>
+        <h1>Shopping List</h1>
+        <button id = "reset">Reset</button>
+        <button id = "clear">Clear</button>
+        {groceryArray.map((grocery) =>(
+        <div class = "block">
+            <p>{grocery.name}</p>
+            <p>{grocery.quantity}{grocery.unit}</p>
+            <button id="buy">Buy</button>
+            <button id="remove">Remove</button>
+        </div>
+             ))}
+    </>
+)
+
+}
+
+export default GroceryList
