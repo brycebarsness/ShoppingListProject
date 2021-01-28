23685389810768fb34b13474bc47f6ba866120d1
mitchellscot <Mitchellscott@me.com>
2021-01-28 11:19:49 -0600

    got the get request on the router

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.get('/', (req, res) => {
+    let query = 'SELECT * FROM "groceries" ORDER BY "id" ASC;';
+    pool.query(query).then(result => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('error getting grocery list', error);
+      res.sendStatus(500);
+    });
+  });
 
 module.exports = router;
