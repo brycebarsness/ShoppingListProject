43561b61c988e4dc21002303b5c5bffb556d7b27
mitchellscot <Mitchellscott@me.com>
2021-01-28 14:37:24 -0600

    fixed the post route

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,14 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
-// TODO - Add routes here...
 router.post('/', (req,res) => {
     const item = req.body
-    console.log(req.body);
-    const queryText  = `INSERT INTO "groceries" ("name", "quantity", "unit")
-                    VALUES ($1, $2, $3);`;
-    console.log(queryText, [item.name, item.quantity, item.unit]);
-    pool.query(queryText)
+    const queryText  = `INSERT INTO "groceries" ("name", "quantity", "unit") VALUES ($1, $2, $3);`;
+    pool.query(queryText, [item.name, item.quantity, item.unit])
     .then((result) => {
         console.log('Added Item to database', item);
         res.sendStatus(201);
