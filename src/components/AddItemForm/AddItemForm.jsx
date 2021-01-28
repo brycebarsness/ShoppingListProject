986abe717166589048220c6fbf0a0a5a42957ba2
mitchellscot <Mitchellscott@me.com>
2021-01-28 13:57:19 -0600

    for some reason the folder didn't get sent

---
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -0,0 +1,20 @@
+function addItemForm(props) {
+
+    return(
+        <form onSubmit={props.addItem}>
+            <label htmlFor="itemInput">Item:</label>
+            <input id="itemNameInput" value={props.newItemName}
+            required></input>
+            <label htmlFor="quantityInput">Quantity</label>
+            <input id="quantityInput" value={props.newItemQuantity} required></input> 
+            <label htmlFor="unitInput">Quantity</label>
+            <input id="unitInput" value={props.newItemUnit} required></input> 
+            <button type="submit">Save</button>
+        </form>
+    );
+}
+
+export default addItemForm;
+
+//TODO addItem pass in as props
+//newItem props
