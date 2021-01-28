144458ae70913e8de7fadc952f9fd57f88445aa0
Milton Fernandez <miltonfernandez17@yahoo.com>
2021-01-28 15:37:00 -0600

    worked on put request

--- src/components/GroceryList/GroceryList.jsx
+++ src/components/GroceryList/GroceryList.jsx
@@ -10,8 +10,13 @@ return(
         <div class = "block">
             <p>{grocery.name}</p>
             <p>{grocery.quantity}{grocery.unit}</p>
-            <button id="buy">Buy</button>
-            <button id="remove">Remove</button>
+            {grocery.purchase ? 
+            <p>Purchased</p>:
+            <div> 
+                <button data-buyid={grocery.id}>Buy</button>
+                <button data-removeid={grocery.id}>Remove</button>
+            </div>
+            }
         </div>
              ))}
     </>
