a4055f3e7ec241af56a5bb3ce77c26c7906c4f63
mitchellscot <Mitchellscott@me.com>
2021-01-28 15:00:43 -0600

    made a component and fixed up the form a bit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -60,21 +60,10 @@ function App() {
                 newItemUnit={newItemUnit}
                 setNewItemUnit={setNewItemUnit}
             />
-            
-                <p>Under Construction...</p>
-                <h1>Shopping List</h1>
-                <button id="reset">Reset</button>
-                <button id="clear">Clear</button>
-                <br></br>
-                <br></br>
-                {groceryArray.map((grocery) => (
-                    <div class="block">
-                        <p>{grocery.name}</p>
-                        <p>{grocery.quantity}{grocery.unit}</p>
-                        <button id="buy">Buy</button>
-                        <button id="remove">Remove</button>
-                    </div>
-                ))}
+            <GroceryList 
+            groceryArray={groceryArray}
+            />
+
             
             </main>
         </div>
