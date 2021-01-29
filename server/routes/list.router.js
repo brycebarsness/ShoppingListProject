340913881acc7bcd7d54b925cd86e0f2e8187905
mitchellscot <Mitchellscott@me.com>
2021-01-29 11:19:09 -0600

    added a readme template

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,6 +5,7 @@ const pool = require('../modules/pool.js');
 router.post('/', (req,res) => {
     const item = req.body
     const queryText  = `INSERT INTO "groceries" ("name", "quantity", "unit") VALUES ($1, $2, $3);`;
+    
     pool.query(queryText, [item.name, item.quantity, item.unit])
     .then((result) => {
         console.log('Added Item to database', item);
